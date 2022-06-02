5cb140331bf2ab774c7cc3b2effa81e2172df910
jpcullinane401 <jpaulcull@gmail.com>
2022-06-02 13:02:40 -0400

    something

--- src/javastuffp/JavaStuff.java
+++ src/javastuffp/JavaStuff.java
@@ -1,8 +1,25 @@
 package javastuffp;
 
 public class JavaStuff {
+	
+	static boolean ihateyou;
+	boolean bruh;
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hellodffo World");
+		
+		
+		
 	}
+	
+	public static void sayBruh() {
+		System.out.println("Bruh");
+	}
+	
+	public static boolean bruhIf(boolean bruh) {
+		return bruh;
+		
+	}
+	
 }
